45a3363b10de02e3bac39e125f94ec1d38a1cdaa
Mirko Mälicke <mirko@hydrocode.de>
2024-02-12 08:07:02 +0100

    add upload function

---
+++ storage/app.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, UploadFile
+import time
+
+from .settings import settings
+from .__version__ import __version__
+
+app = FastAPI(
+    title="Deadwood-AI Storage API",
+    description="This is the Deadwood-AI Storage API. It is used to manage files uploads for the Deadwood-AI backend.",
+    version=__version__,
+)
+
+
+@app.get("/")
+def info():
+    """
+    Get information about the storage API.
+    """
+    return {
+        "name": "Deadwood-AI Storage API.",
+        "description": "This is the Deadwood-AI Storage API. It is used to manage files uploads for the Deadwood-AI backend. If you are not a developer, you may be searching for https://deadtrees.earth",
+    }
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    """
+    Upload a file to the server.
+
+    This endpoint expects a single file to be uploaded. The file will be saved to the raw upload directory and 
+    the following information about the file will be returned:
+
+    - filename: the name of the file
+    - content_type: the MIME type of the file
+    - file_size: the size of the file in bytes
+    - target_path: the path where the file was saved
+    - copy_time: the time it took to save the file in seconds
+
+    To send the file use the `multipart/form-data` content type. The file should be sent as the value of a field
+    named `file`. For example, using HTML forms like this:
+
+    ```html
+    <form action="/upload" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit">
+    </form>
+    ```
+
+    Or using the `requests` library in Python like this:
+
+    ```python
+    import requests
+    url = "http://localhost:8000/upload"
+    files = {"file": open("example.txt", "rb")}
+    response = requests.post(url, files=files)
+    print(response.json())
+    ```
+
+    """
+    # save the file to the raw upload directory
+    target_path = settings.raw_upload_path / file.filename
+
+    # check if the file already exists
+    # TODO: what do we actually want here? Overwrite? Rename? Error?
+    if target_path.exists():
+        raise ValueError(f"file already exists: {file.filename}")
+    
+    # write the file to the target path
+    t1 = time.time()
+    with target_path.open("wb") as buffer:
+        buffer.write(await file.read())
+    t2 = time.time()
+
+    # finally return some info about the uploaded file
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "file_size": file.size,
+        "target_path": str(target_path),
+        "copy_time": t2 - t1,
+    }
