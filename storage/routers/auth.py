f848a95c2e97a890387b3023608844f525cddbe8
Mirko Mälicke <mirko@hydrocode.de>
2024-02-20 11:42:14 +0100

    token function to validate supabase access_tokens

---
+++ storage/routers/auth.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+
+from ..supabase_auth import login as supabase_login
+
+router = APIRouter()
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    # use the supabase login to verify the token there
+    response = supabase_login(form_data.username, form_data.password)
+
+    # return the response as FastAPI needs it
+    return {"access_token": response.session.access_token, "token_type": "bearer"}
